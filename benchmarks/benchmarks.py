d91ce1c7ce17392ad9b083616fcaabb1536c44a7
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-11-17 14:23:12 +0100

    Update benchmarks

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,5 +1,11 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
+import os
+from shutil import rmtree
+
+import numpy as np
+import z5py
+import z5py.util
 
 
 class TimeSuite:
@@ -7,30 +13,48 @@ class TimeSuite:
     An example benchmark that times the performance of various kinds
     of iterating over dictionaries in Python.
     """
+    tmp_folder = './tmp'
+    file_read = './tmp/file_read.n5'
+    file_write = './tmp/file_write.n5'
+
     def setup(self):
-        self.d = {}
-        for x in range(500):
-            self.d[x] = None
+        self.data = z5py.util.fetch_test_data_stent()
+        self.chunks = (64,) * 3
+        try:
+            os.mkdir(self.tmp_folder)
+        except OSError:
+            pass
+        with z5py.File(self.file_read) as f:
+            f.create_dataset('data_gzip', data=self.data,
+                             compression='gzip', chunks=self.chunks)
+            f.create_dataset('data_raw', data=self.data,
+                             compression='raw', chunks=self.chunks)
 
-    def time_keys(self):
-        for key in self.d.keys():
+    def teardown(self):
+        try:
+            rmtree(self.tmp_folder)
+        except OSError:
             pass
 
-    def time_iterkeys(self):
-        for key in self.d.iterkeys():
-            pass
+    def time_write_raw(self):
+        with z5py.File(self.file_write) as f:
+            f.create_dataset('data_raw', data=self.data,
+                             compression='raw', chunks=self.chunks)
 
-    def time_range(self):
-        d = self.d
-        for key in range(500):
-            x = d[key]
+    def time_write_gzip(self):
+        with z5py.File(self.file_write) as f:
+            f.create_dataset('data_gzip', data=self.data,
+                             compression='gzip', chunks=self.chunks)
 
-    def time_xrange(self):
-        d = self.d
-        for key in xrange(500):
-            x = d[key]
+    def time_read_gzip(self):
+        with z5py.File(self.file_read) as f:
+            data = f['data_gzip'][:]
+
+    def time_read_raw(self):
+        with z5py.File(self.file_read) as f:
+            data = f['data_raw'][:]
 
 
-class MemSuite:
-    def mem_list(self):
-        return [0] * 256
+# class MemSuite:
+#     def mem_list(self):
+#         return [0] * 256
